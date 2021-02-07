67516b39f545b7f97983a9fa0a2320e69f08c1e1
Maggie S <Maggiesaldivia@gmail.com>
2021-02-06 17:57:34 -0800

    confirm that spec test 3 works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
-//spec test 2
+//spec test 3
+
 let vowels = ['a', 'e', 'i', 'o', 'u']
+let result = " "
 function pigLatin(word) {
   if (vowels.includes(word[0]) === true) {
-    word.concat('way')
-  }
+    result = word.concat('way')
+    return result
+  } else if ((vowels.includes(word[0]) === false) || (vowels.includes(word[1]) === false)) {
+    alert("this works")
+  }    
 }
 
-console.log(pigLatin('a'))
+
+pigLatin("chair")
