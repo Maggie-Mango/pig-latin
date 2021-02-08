736756913d52a465b4cd0ca681f11c6d3bff41fe
Maggie S <Maggiesaldivia@gmail.com>
2021-02-07 17:40:39 -0800

    fix function index slicing and add to the Readme.

--- README.md
+++ README.md
@@ -13,7 +13,10 @@
 
 
 ## Description
-
+* For words beginning with a vowel, add "way" to the end.
+* For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+* If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+* For words beginning with "y", treat "y" as a consonant.
 
 
 ## Setup/Installation Requirements
@@ -32,7 +35,7 @@ Unavailable until more features are added
 |:--------:|:--------:|:--------:|
 |`pigLatin()`| "It will check for words beginning with a vowel" | (pigLatin("a")).toEqual(true)) |
 |`pigLatin()`| "It will add 'way' to the end of words that begin with a vowel" | (pigLatin("a")).toEqual("away") |
-|`pigLatin()`| "It will move the first one or two consonants to the end and add 'way'" | (pigLatin("chair")).toEqual("true") |
+|`pigLatin()`| "It will move the first one or two consonants to the end and add 'way'" | (pigLatin("chair")).toEqual("airchay") |
 |`pigLatin()`| "It will check for the first one or two consonants that also include 'qu'" | (pigLatin("squeal")).toEqual("true") |
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,11 @@ let vowels = ['a', 'e', 'i', 'o', 'u']
 function pigLatin(word) {
   let result = ""
   let vowelIndex = 0
+  //checks if first character is a vowel
   if (vowels.includes(word[0]) === true) {
     result = word.concat('way')
     return result
-  //first letter is consonant
+  //checks to see if more than one word is inserted
   } else if (word.includes(" ")) {
     alert("this looks like more than one word... try again.");
     $("#resetbtn").show();
@@ -15,14 +16,17 @@ function pigLatin(word) {
   }
   
     else {
+    //loops through inputted word until they hit a vowel
     for (let i of word) {
       if (vowels.includes(i)) {
         vowelIndex = word.indexOf(i);
-      if(word.slice(i, i+1) === 'qu') i += 1;
+      //loops through inputted word until it hits `qu` and then adds 1 to the i
+      if (word.slice(i, i+1) === 'qu') i += 1;
       }
     }
-    // construct the sentence
-    result = word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay"
+    // construct the sentence (ex: "chair" is broken down as => "air" + "ch" + "ay")
+    result = word.slice(vowelIndex-1) + word.slice(0, vowelIndex-1) + "ay"
+
     return result
   }    
 }
@@ -40,9 +44,7 @@ $(document).ready(function() {
 $(function () {
   $("#resetbtn").on("click", function (e) {
       e.preventDefault();
-     // $('#theForm')[0].reset(); // Or
       $('form#formInput').trigger("reset");
       $("#resetbtn").hide();
-   
   });
 });
